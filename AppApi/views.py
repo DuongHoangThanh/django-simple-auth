6c8a919b079b2ad29560bdbd2b2719ae7d32466f
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2024-06-11 00:54:15 +0700

    Add: Update & Delete Task

--- AppApi/views.py
+++ AppApi/views.py
@@ -8,7 +8,6 @@ from .serializers import TaskSerializer
 
 class TaskList(APIView):
     permission_classes = [IsAuthenticated]
-    
     def get(self, request):
         tasks = Task.objects.filter(user=request.user)
         serializer = TaskSerializer(tasks, many=True)
@@ -23,4 +22,15 @@ class TaskList(APIView):
             return Response(serializer.data, status=201)        
         return Response(serializer.errors, status=400)
 
-
+    def put(self, request, pk=None):
+        task = Task.objects.get(id=pk, user = request.user)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
+    
+    def delete(self, request , pk=None):
+        task = Task.objects.get(id=pk, user = request.user)
+        task.delete()
+        return Response(status=204)
